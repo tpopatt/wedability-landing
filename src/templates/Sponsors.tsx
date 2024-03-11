9b883c5d3a3587b621c3a63a829ddc07a1654c77
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-03-10 21:57:18 -0400

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -8,7 +8,11 @@ const Sponsors = () => (
       alt="Better Stack"
       width={2900}
       height={2290}
-      className='rounded-lg px-4 drop-shadow-xl md:px-32'
+      className='rounded-lg px-4'
+      style={{
+        // shadow in all directions
+        boxShadow: '10px 10px rgba(0, 0, 0, 0.1)',
+      }}
     />
 );
 
